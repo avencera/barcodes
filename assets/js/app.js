285cd934bf01db81f0699e8f351bf302255f856e
Praveen Perera <ppraveen25@gmail.com>
2019-03-29 12:33:05 -0400

    Fix barcode image generation on click

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,15 +7,16 @@ window.jQuery = $;
 window.$ = $;
 import Base64 from "base-64";
 
-JsBarcode(".barcode").init();
+JsBarcode(".barcode-canvas").init();
+JsBarcode(".barcode-svg").init();
 
-const barcodes = document.getElementsByClassName("barcode");
+const barcodes = document.getElementsByClassName("barcode-data");
 
 Array.prototype.forEach.call(barcodes, function(barcode) {
   const image_link = document.getElementById(`image-link-${barcode.id}`);
   const svg_link = document.getElementById(`svg-link-${barcode.id}`);
 
-  const dataURL = barcode.toDataURL();
+  const dataURL = document.getElementById(`canvas-${barcode.id}`).toDataURL();
   const svg = document.getElementById(`svg-${barcode.id}`);
 
   const b64 = Base64.encode(svg.outerHTML);
